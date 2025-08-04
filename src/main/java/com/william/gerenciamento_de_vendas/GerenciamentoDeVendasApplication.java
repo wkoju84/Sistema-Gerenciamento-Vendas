3c1ceb2889b01f471e70b01a85646f0208152209
wkoju84 <wkoju84@gmail.com>
2025-08-04 18:17:09 -0300

    commit inicial

---
+++ src/main/java/com/william/gerenciamento_de_vendas/GerenciamentoDeVendasApplication.java
@@ -0,0 +1,13 @@
+package com.william.gerenciamento_de_vendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentoDeVendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentoDeVendasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/william/gerenciamento_de_vendas/GerenciamentoDeVendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.william.gerenciamento_de_vendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoDeVendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
