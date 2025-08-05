341443de7cc9df25fcb917ff0f9eae3847eb2737
wkoju84 <wkoju84@gmail.com>
2025-08-05 14:12:52 -0300

    Criando UserRepository e adicionando findByLogin

---
+++ src/main/java/com/william/gerenciamento_de_vendas/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.william.gerenciamento_de_vendas.repositories;
+
+import com.william.gerenciamento_de_vendas.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface UserRepository extends JpaRepository<User, String> {
+
+    UserDetails findByLogin(String login);
+}
