1fcaa3547ec07a7479c3513c9b6318186b150c20
wkoju84 <wkoju84@gmail.com>
2025-08-09 21:45:17 -0300

    chore(security): atualizar configurações de segurança
    Ajusta parâmetros e políticas de segurança do sistema para alinhar com as
    novas recomendações internas. Inclui atualização de propriedades de
    configuração e revisão de regras de autenticação e autorização.
    
    Refs: #789

--- src/main/java/com/william/gerenciamento_de_vendas/infrastructure/security/SecurityConfig.java
+++ src/main/java/com/william/gerenciamento_de_vendas/infrastructure/security/SecurityConfig.java
@@ -24,6 +24,8 @@ public class SecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(
                         SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
                         .requestMatchers(HttpMethod.POST, "/product").hasRole("ADMIN")
                         .anyRequest().authenticated()
                 )
